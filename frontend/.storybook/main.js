21c2a478da53d5f93d2d2d3d773246909a6e995d
aslink87 <adam.slinkman@gmail.com>
2022-08-12 08:42:11 -0400

    feat: added storybook, tailwind, husky, commitlint, eslint, prettier

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-scss',
+    'storybook-addon-next-router',
+    '@storybook/addon-next',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+    disableTelemetry: true,
+  },
+};
