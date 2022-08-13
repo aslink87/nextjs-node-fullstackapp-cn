92d417c0d8054fb8b0300f8b97cedc8348137e38
aslink87 <adam.slinkman@gmail.com>
2022-08-13 19:44:35 -0400

    feat: added news api

---
+++ lib/types.ts
@@ -0,0 +1,7 @@
+export interface INews {
+  id: string,
+  title: string,
+  content: string,
+  link?: string,
+  video?: string
+}
---
+++ pages/api/news.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma'
+import { INews } from '../../lib/types';
+
+interface INewsData extends NextApiRequest {
+  body: INews
+}
+
+export default async function handler(
+  req: INewsData,
+  res: NextApiResponse
+) {
+
+  // update News object
+  const updateNews = async () => {
+    const news = await prisma.news.update({
+      where: { index: 1 },
+      data: {
+        title: req.body.title,
+        content: req.body.content,
+        link: req.body.link,
+        video: req.body.video
+      }
+    })
+    return res.status(200).json(news)
+  }
+
+
+  if (req.method === 'POST' && req.body.title.length >= 3 && req.body.content.length > 5) {
+    updateNews()
+  } else {
+    return res.status(400).json('Unable to update News post')
+  }
+}
