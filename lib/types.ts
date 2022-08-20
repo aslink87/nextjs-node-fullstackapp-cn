c0115c149649512437b31572bec9c0473ced89c0
aslink87 <adam.slinkman@gmail.com>
2022-08-20 19:42:18 -0400

    feat: added pantryNeeds schema and api

--- lib/types.ts
+++ lib/types.ts
@@ -38,3 +38,17 @@ export interface IBlog {
   link?: string,
   img?: string
 }
+
+export interface INeeds {
+  id: string,
+  item0?: string,
+  item1?: string,
+  item2?: string,
+  item3?: string,
+  item4?: string,
+  item5?: string,
+  item6?: string,
+  item7?: string,
+  item8?: string,
+  item9?: string,
+}
