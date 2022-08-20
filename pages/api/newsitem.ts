191668df5e84f7e98f3d51806d01f7b2c110bab2
aslink87 <adam.slinkman@gmail.com>
2022-08-20 19:27:16 -0400

    feat: added NewsItem schema and api

---
+++ pages/api/newsitem.ts
@@ -0,0 +1,73 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma'
+import { IPdf } from '../../lib/types';
+import formidable from 'formidable';
+
+interface INewsItemData extends NextApiRequest {
+  body: IPdf
+}
+
+// turn off body parser so that formData is consumed properly
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default async function handler(
+  req: INewsItemData,
+  res: NextApiResponse
+) {
+
+  // prismaHandler updates DB object values including new filePath
+  const prismaHandler = async (pathFile: string) => {
+    try {
+      await prisma.newsitem.update({
+        where: { index: 1 },
+        data: {
+          doc: pathFile,
+        }
+      })
+      res.status(200).json({ message: 'File Uploaded Successfully' })
+    } catch (error) {
+      return res.status(500).json({ message: 'Error updating server with new image' })
+    }
+  }
+
+  const updateNewsItem = async () => {
+    try {
+      const options = {
+        uploadDir: './uploads'
+      }
+
+      // parse incoming document and return filepath to pass on to prismaHandler
+      const form = new formidable.IncomingForm(options);
+      let pathFile = ""
+      await new Promise(function(resolve, reject) {
+        form.parse(req, (error, field, file) => {
+          if (error) {
+            res.status(500).json({ message: 'Error parsing file' });
+            return reject(error);
+          }
+          const path: any = file[Object.keys(file)[0]]
+          let filepath = path.filepath
+          pathFile = filepath
+          resolve({ file, field })
+        })
+      })
+      prismaHandler(pathFile)
+    }
+    catch (error) {
+      console.log(error)
+      return res.status(500).send('Upload Error' + error)
+    }
+
+  }
+
+  if (req.method === 'POST') {
+    updateNewsItem()
+  } else {
+    return res.status(400).json({ message: 'Unable to update News Item' })
+  }
+}
+
